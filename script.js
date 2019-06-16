7d1947efd6568c292a3644bce0f1f5f4645d3412
Mirzayev085 <49615672+Mirzayev085@users.noreply.github.com>
2019-06-16 12:14:01 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -72,8 +72,8 @@ myButton.style.width = "150px";
 myButton.style.height = "40px";
 myButton.style.color = "white"
 myButton.style.backgroundColor = "orange";
-myButton.style.border = "0px";
-myButton.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+myButton.style.border = "2px solid white";
+myButton.style.boxShadow = "2px 2px orange";
 
 
 //MY HEADLINE
